95e90bd54517e3734ae35e329c7f957660b8a590
Aleksei Sheifler <a.a.shejfler@gmail.com>
2019-06-15 23:59:41 +0300

    refactoring

--- src/main/kotlin/org/rootbr/camunda/spin/gson/format/GsonDataFormat.kt
+++ src/main/kotlin/org/rootbr/camunda/spin/gson/format/GsonDataFormat.kt
@@ -12,7 +12,6 @@ import org.camunda.spin.json.SpinJsonNode
 import org.camunda.spin.spi.DataFormat
 import org.rootbr.camunda.spin.gson.GsonNode
 
-
 class GsonDataFormat
 @JvmOverloads constructor(
         protected val nameDataFormat: String = DataFormats.JSON_DATAFORMAT_NAME,
@@ -22,34 +21,15 @@ class GsonDataFormat
                 .mappingProvider(GsonMappingProvider(gson))
                 .build()
 ) : DataFormat<SpinJsonNode> {
-
     protected var dataFormatReader = GsonDataFormatReader(this)
     protected var dataFormatWriter = GsonDataFormatWriter(this)
     protected var dataFormatMapper = GsonDataFormatMapper(this)
-
     override fun getName() = nameDataFormat
-
-    override fun getWrapperType(): Class<out SpinJsonNode> {
-        return GsonNode::class.java
-    }
-
-    override fun createWrapperInstance(parameter: Any): SpinJsonNode {
-        val gsonNode = GsonNode(createGsonNode(parameter), this)
-        return gsonNode
-    }
-
-    override fun getMapper(): GsonDataFormatMapper {
-        return dataFormatMapper
-    }
-
-    override fun getReader(): GsonDataFormatReader {
-        return dataFormatReader
-    }
-
-    override fun getWriter(): GsonDataFormatWriter {
-        return dataFormatWriter
-    }
-
+    override fun getWrapperType(): Class<out SpinJsonNode> = GsonNode::class.java
+    override fun createWrapperInstance(parameter: Any): SpinJsonNode  = GsonNode(createGsonNode(parameter), this)
+    override fun getMapper() =  dataFormatMapper
+    override fun getReader()= dataFormatReader
+    override fun getWriter()= dataFormatWriter
     fun createGsonNode(parameter: Any?) = when (parameter) {
         is SpinJsonNode -> parameter.unwrap() as JsonElement
         else -> gson.toJsonTree(parameter)
