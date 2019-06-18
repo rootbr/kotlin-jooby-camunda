d3da806dedbb2ad4b07628baa0e167d4b5da4ba3
Aleksei Sheifler <a.a.shejfler@gmail.com>
2019-06-18 22:21:00 +0300

    change front to vue.js

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import pkg from './package.json';
+
+import cjs from 'rollup-plugin-commonjs';
+import vue from 'rollup-plugin-vue';
+
+function pgl() {
+  return [
+    cjs(),
+    vue()
+  ];
+}
+
+export default [
+  {
+    input: './src/main/javascript/index.vue',
+    output: {
+      name: 'VueBpmn',
+      file: `public/js/vue-bpmn.umd.js`,
+      format: 'umd'
+    },
+    plugins: pgl()
+  },
+  {
+    input: './src/main/javascript/index.vue',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: pgl()
+  }
+];
